51b1ad3602f542b207bf5ace9cb92dd74f9c14a6
elio-m <elio.mine4@gmail.com>
2020-02-12 16:00:38 +0100

    creating an http server

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
+const server =  http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.end('<h1>Home</h1>');
+    }
+});
 
-logger.on('message', data => console.log('Called Listener:', data));
+const PORT = process.env.PORT || 5000;  //look for environment variable, if not found then run port 5000
 
-logger.log('Hello World');
-
-
-// const Person = require('./person');
-//
-// const person1 = new Person('john doe', 30);
-//
-// person1.greetings();
-
+server.listen( PORT, () => console.log(`Server running on port ${PORT}`));
--- logger.js
+++ logger.js
@@ -10,4 +10,19 @@ class logger extends EventEmitter
     }
 }
 
-module.exports = logger;
+// module.exports = logger;
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', data => console.log('Called Listener:', data));
+
+logger.log('Hello World');
+
+
+// const Person = require('./person');
+//
+// const person1 = new Person('john doe', 30);
+//
+// person1.greetings();
