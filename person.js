1614abde16011e1c525dbd65f5a83ce070e5559e
elio-m <elio.mine4@gmail.com>
2020-02-12 10:20:36 +0100

    creating the first class

---
+++ person.js
@@ -0,0 +1,13 @@
+class Person {
+    constructor(name , age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greetings()
+    {
+        console.log(`My name is ${this.name} and i am ${this.age}`);
+    }
+}
+
+module.exports = Person;
